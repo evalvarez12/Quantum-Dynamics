daffbb4123611f04f82b7892591f8e32c1b9da9e
Eduardo <evalvarez12@gmail.com>
2017-04-25 16:58:34 +0200

    Playing with doubleSlit

--- doubleSlit.py
+++ doubleSlit.py
@@ -13,7 +13,7 @@ import simulation as sm
 
 # Define the system parameters
 dim = 2
-numberPoints = 128
+numberPoints = 64
 dt = .001
 dirichletBC = False
 startPoint = [0, 0]
@@ -29,8 +29,14 @@ y = np.linspace(startPoint[1], startPoint[1] + domainLength, allPoints).reshape(
 
 
 def doubleSlit(x, y):
-    if x > 0.4 and x < 0.5 and (y < 0.8 or y > 1.2):
-        return 5000
+    sS = .3
+    sW = .1
+    spX = .5
+    spY = 1
+    if x > spX and x < spX+.05 and (y < spY-sS/2. or y > spY+sS/2.):
+        return 50000
+    if x > spX and x < spX+.05 and (y > spY-sS/2.+sW and y < spY+sS/2.-sW):
+        return 50000
     else:
         return 0
 
@@ -42,7 +48,7 @@ sim = sm.Simulation(dim=dim, potentialFunc=doubleSlit,
                     dt=dt)
 
 # Create the initial wave function
-sim.setPsiPulse(energy=500, center=.2, width=.1)
+sim.setPsiPulse(energy=500, center=.1, width=.1)
 # plt.show()
 
 
@@ -50,15 +56,8 @@ sim.setPsiPulse(energy=500, center=.2, width=.1)
 fig = plt.figure()
 im = plt.imshow(np.transpose(sim.normPsi().reshape(allPoints, allPoints)),
                 animated=True, cmap=plt.get_cmap('jet'))
-# plt.show()
-plt.colorbar()
-plt.imshow(np.vectorize(doubleSlit)(x, y), cmap=plt.get_cmap('rainbow'), alpha=1)
-plt.colorbar()
 
-# line, = ax1.plot(x, sim.realPsi())
-# ax1.set_xlabel(r'$x$')
-# ax1.set_ylabel(r'$y$')
-# ax1.tick_params('y', colors='b')
+# plt.imshow(np.vectorize(doubleSlit)(x, y), cmap=plt.get_cmap('rainbow'), alpha=1)
 
 def animate(i):
     global sim  # Breaks the animation when used in a function
@@ -67,11 +66,5 @@ def animate(i):
     return im,
 
 ani = animation.FuncAnimation(fig, animate, frames=600, interval=10, blit=True)
-
-# if not V == 'none':
-#     ax2 = ax1.twinx()
-#     ax2.imshow(V(x, y))
-#     # ax2.set_ylabel('$V(x)$')
-#     # ax2.tick_params('y', colors='r')
 
 plt.show()
