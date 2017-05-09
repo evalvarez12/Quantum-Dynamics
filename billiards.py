d1f51ffb60f34b2eb6ec1df4fe36d9f2a5dcacfa
Eduardo <evalvarez12@gmail.com>
2017-05-09 02:35:18 +0200

    Modified billiards

--- billiards.py
+++ billiards.py
@@ -21,49 +21,30 @@ startPoint = [0, 0]
 domainLength = 1
 
 
-def dispersion(x, y):
-    center = [0.5, 0.5]
-    r = np.linalg.norm([x - center[0], y - center[1]])
-    return 15./r**2
-
-
-def dispersionVis(x, y):
+def lorentzGas(x, y):
+    '''Potential corresponding to the Lorentz Gas billiard'''
     center = [0.5, 0.5]
     r = np.linalg.norm([x - center[0], y - center[1]])
     if r < .2:
-        return 5000
+        return 20000
     else:
         return 0
 
 
 # Create the simulation for the system
-sim = sm.Simulation(dim=dim, potentialFunc=dispersionVis,
+sim = sm.Simulation(dim=dim, potentialFunc=lorentzGas,
                     dirichletBC=dirichletBC, numberPoints=numberPoints,
                     startPoint=startPoint, domainLength=domainLength,
                     dt=dt)
 
-# Create the initial wave function
-# Psi = np.zeros((numberPoints + sign)**2)
-#location = [np.random.choice(x, size = numPulses),
-#            np.random.choice(y.flatten(), size = numPulses)]
-# location = np.array([np.random.choice(x, size = numPulses),
-            # np.random.choice(y.flatten(), size = numPulses)])
-# location = np.swapaxes(location, 0, 1)
-vel = np.random.uniform(low=-1, high=1, size=(2, 1))
 
-# for i in range(numPulses):
-#     sim.setPsiPulse(energy=500, center=location[i], vel_x=vel[0, i],
-#                     vel_y=vel[1, i], width=.1)
-#     Psi = Psi + sim.psi
-
-# sim.psi = Psi
-sim.setPsiPulse(pulse="circular", energy=500, center=[.2, .5], vel=[.4, .8],
+sim.setPsiPulse(pulse="circular", energy=1000, center=[.3, .2], vel=[.4, .1],
                 width=.1)
 
-for i in range(30):
+for i in range(50):
     sim.evolve()
 
 # System evolution and Animation
-ani = qplots.animation2D(sim, psi="norm", potentialFunc=dispersionVis,
+ani = qplots.animation2D(sim, psi="norm", potentialFunc=lorentzGas,
                          save=False)
 plt.show()
